93ccde5a536a81bb7e9039382ff68ea08675bf17
quannguyen0510 <sorenbjerg1103@gmail.com>
2019-07-24 12:02:36 +0700

    Create news, salaries table and model, modified user table

---
+++ database/migrations/2019_07_24_042526_create_salaries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->unsignedInteger('id_user');
+            $table->unsignedInteger('year');
+            $table->unsignedInteger('month');
+            $table->integer('day_worked');
+            $table->integer('bonus_project');
+            $table->integer('total');
+            $table->string('checklist');
+            $table->primary(['id_user', 'year', 'month']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salaries');
+    }
+}
